ca4a67109429452b7b316018a1df5b236e46a848
fl0ppydiskk <florine.schoenmann@gmail.com>
2025-08-01 18:35:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -194,4 +194,5 @@ def flight_info():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
